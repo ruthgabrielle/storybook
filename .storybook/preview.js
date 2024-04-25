ef942616e450823cbb8e77beca99c2ca1d1e94be
Ruth Gomes <rgabrielle@uolinc.com>
2024-04-25 09:11:38 -0300

    theme story

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import ThemeProvider from "../src/theme/ThemeProvider";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*"},
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/i,
+    },
+  },
+};
+
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider>
+    <Story /> 
+    </ThemeProvider>
+  )
+]
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,20 +6,18 @@ const StyledButton = styled.button<ButtonProps>`
   border: 0;
   padding: 8px 14px;
   transition: 0.3s;
-
   ${({ theme, variant }) => {
-    console.log(theme);
-
+    /* console.log(theme); */
     return {
       backgroundColor: theme.colors[variant].main,
       color: theme.colors[variant].text,
-      ':hover': {
+      ":hover": {
         backgroundColor: theme.colors[variant].light,
       },
-      ':focus': {
+      ":focus": {
         backgroundColor: theme.colors[variant].dark,
-      }
-    }
+      },
+    };
   }}
 `;
 
